fa242e50a62f7e46561f03413ed06fbc7cdb41d8
GrailSeeker <pranaydora@gmail.com>
2022-05-14 20:13:48 +0530

    Added Kubernetes deployment files with Helm.

--- PokEBay/PokEBay.Notifications.API/Infrastructure/NotificationService.cs
+++ PokEBay/PokEBay.Notifications.API/Infrastructure/NotificationService.cs
@@ -2,9 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using PokEBay.Notifications.API.Infrastructure.DTO;
-using SendGrid;
-using SendGrid.Helpers.Mail;
-using System;
 using System.Threading.Tasks;
 
 namespace PokEBay.Notifications.API.Infrastructure
@@ -26,23 +23,6 @@ namespace PokEBay.Notifications.API.Infrastructure
         {
             // Send notification here.
             await Task.Run(() => _logger.LogInformation($"***Your dummy email.*** \n Order# {orderDto.Id} was successfully placed. \n *****"));
-
-            try
-            {
-                var apiKey = "[SendGrid Key]";
-                var client = new SendGridClient(apiKey);
-                var from = new EmailAddress("test@example.com", "Example User");
-                var subject = "Sending with SendGrid is Fun";
-                var to = new EmailAddress("test@example.com", "Example User");
-                var plainTextContent = "and easy to do anywhere, even with C#";
-                var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-                var response = await client.SendEmailAsync(msg);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError($"Sending email to endGrid failed with the following error(s): \n {ex.Message} \n {ex.InnerException?.Message}");
-            }
         }
     }
 }
